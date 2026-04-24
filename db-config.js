2baf4ed8c2d56c5b7ea5c1f3dcacdbe475698e25
Yogii faujdar <mehar@Yogiis-MacBook-Pro.local>
2026-04-24 17:22:24 +0530

    fix: disable SSL for postgres connection as server does not support it

--- db-config.js
+++ db-config.js
@@ -1,4 +1,4 @@
 module.exports = {
   connectionString: process.env.DATABASE_URL || 'postgres://chauhan:chauhan%40computer@187.77.187.120:5411/chauhan%20db',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: false
 };
